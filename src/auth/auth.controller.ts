60afec1e209b3241622f44b74c389322731962b3
David <datogio@gmail.com>
2022-12-20 14:10:39 +0400

    feat: encrypting and returning jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @UseGuards(AuthGuard('local'))
   @Post('login')
   async login(@Request() req) {
-    return req.user;
+    const jwt = await this.authService.login(req.user);
+    return { data: jwt, meta: {} };
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { IUser } from 'src/users/users.model';
 import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUse(email: string, password: string): Promise<Object | null> {
     const user = await this.usersService.findOne(email);
@@ -12,5 +16,12 @@ export class AuthService {
       return { id: user.id, email: user.email } as IUser;
     }
     return null;
+  }
+
+  async login(user: { id: string; email: string }) {
+    const payload = { email: user.email, sub: user.id };
+    return {
+      jwt: this.jwtService.sign(payload),
+    };
   }
 }
