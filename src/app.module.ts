fadbd00e3ab22986ddfe23dcd159f66e090b8d72
David <datogio@gmail.com>
2022-12-20 12:49:49 +0400

    build: connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://datogio:apassfortraderN1@cluster0.jjsez4q.mongodb.net/auction?retryWrites=true&w=majority`,
+    ),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
