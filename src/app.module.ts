717a282d744f0680aa1494f6beb639fccef7130d
David <datogio@gmail.com>
2022-12-30 21:46:57 +0400

    build: introduce course module, service & controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { S3Module } from './s3/s3.module';
 import { ConfigModule } from '@nestjs/config';
+import { CourseModule } from './course/course.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { ConfigModule } from '@nestjs/config';
     AuthModule,
     UsersModule,
     S3Module,
+    CourseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
